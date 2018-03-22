b2250f4061ead6dd61dff58572940538104dd48c
Pierre Poulain <pierre.poulain@cupnet.net>
2018-03-22 11:27:35 +0100

    Add version file

---
+++ autoclasswrapper/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'autoclass-wrapper'
+__description__ = 'Simple Python wrapper for autoclass-c.'
+__url__ = 'https://github.com/pierrepo/autoclasswrapper'
+__version__ = '0.1'
+__author__ = 'Pierre Poulain'
+__author_email__ = 'pierre.poulain@cupnet.net'
+__license__ = 'BSD 3-Clause'
+__copyright__ = 'Copyright 2018 Pierre Poulain'
