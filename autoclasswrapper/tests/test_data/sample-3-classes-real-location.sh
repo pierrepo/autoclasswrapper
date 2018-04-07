07c52e34645f37c82a9da22e7df4d8f4e32d4e73
Pierre Poulain <pierre.poulain@cupnet.net>
2018-04-08 00:37:47 +0200

    Add test data

---
+++ autoclasswrapper/tests/test_data/sample-3-classes-real-location.sh
@@ -0,0 +1,2 @@
+autoclass -search sample-3-classes-real-location.db2 sample-3-classes-real-location.hd2 sample-3-classes-real-location.model sample-3-classes-real-location.s-params 
+autoclass -reports sample-3-classes-real-location.results-bin sample-3-classes-real-location.search sample-3-classes-real-location.r-params 
---
+++ autoclasswrapper/tests/test_data/sample-3-classes-real-scalar.sh
@@ -0,0 +1,2 @@
+autoclass -search sample-3-classes-real-scalar.db2 sample-3-classes-real-scalar.hd2 sample-3-classes-real-scalar.model sample-3-classes-real-scalar.s-params 
+autoclass -reports sample-3-classes-real-scalar.results-bin sample-3-classes-real-scalar.search sample-3-classes-real-scalar.r-params 
