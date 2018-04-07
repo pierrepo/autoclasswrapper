8f798b3c4956a0f029abe861d35115a4c2f22595
Pierre Poulain <pierre.poulain@cupnet.net>
2018-04-07 23:28:20 +0200

    Fill and format doctrings

--- autoclasswrapper/output.py
+++ autoclasswrapper/output.py
@@ -8,27 +8,64 @@ import logging
 log = logging.getLogger(__name__)
 
 class Output():
-    """
-    Class to handle autoclass output files and results
+    """Autoclass output files and results
+
+    Parameters
+    ----------
+    root_name : string (default "clust")
+        Root name to generate input files for autoclass.
+        Example: "clust" will lead to "clust.db2", "clust.model"...
+    tolerate_error : bool (default: False)
+        If True, countinue generation of autoclass input files even if an
+        error is encounter.
+        If False, stop at first error.
+
+    Attributes
+    ----------
+    had_error : bool (defaut False)
+        Set to True if an error has been found in the generation of autoclass
+        input files.
+    case_number : int (default 0)
+        Number of cases (i.e. of genes/proteins).
+    class_number : int (default 0)
+        Number of classes (i.e. clusters).
+    stats : Pandas dataframe (default None)
+        Dataframe that contains, for all cases, main class and probability
+        for all classes.
+    df : Pandas dataframe (default None)
+        Dataframe that contains initial input data and associated clusters.
+    experiment_names : list of string (defaut [])
+        List of experiment (conditions) names.
+        Corresponds to columns in the input data.
     """
 
-    def __init__(self, inputfolder='',
-                       tolerate_error=False):
+    def __init__(self,
+                 root_name="clust",
+                 tolerate_error=False):
+        """Object instanciation
         """
-        Object instanciation
-        """
-        self.inputfolder = inputfolder
-
+        self.root_name = root_name
         self.tolerate_error = tolerate_error
         self.had_error = False
-
-        self.cases = []
-        self.classes = []
+        self.case_number = 0
+        self.class_number = 0
+        self.stats = None
+        self.df = None
+        self.experiment_names
 
 
     def handle_error(f):
-        """
-        Handle error during data parsing and formating
+        """Handle error during data parsing and formating
+
+        Function decorator.
+
+        Parameters
+        ----------
+        f : function
+
+        Returns
+        -------
+        try_function : function wrapped into error handler
         """
         def try_function(self, *args, **kwargs):
             if self.tolerate_error or not self.had_error:
@@ -42,15 +79,21 @@ class Output():
 
 
     @handle_error
-    def extract_results(self, case_name='clust.case-data-1'):
-        """
-        Extract results from autoclass
+    def extract_results(self):
+        """Extract results from autoclass
 
+        Results extracted are:
+        - Number of cases (i.e. genes/proteins)
+        - Number of classes (i.e. clusters)
+        - For each case X, most probable class
+        - For each case X, probability belonging to class Y
         """
         log.info("Extracting autoclass results")
-        # first pass: get number of cases and classes
-        self.class_number = 0
-        self.case_number = 0
+        case_name = self.root_name + ".case-data-1"
+        # first pass:
+        # - get number of cases and classes
+        # - create empty dataframe to store class/cluster probability
+        #   for all cases (i.e gene/protein)
         classes = set()
         with open(case_name, 'r') as case_file:
             for line in case_file:
@@ -62,22 +105,20 @@ class Output():
                 classes.add(int(items[1]))
                 self.case_number += 1
             self.class_number = len(classes)
-        log.info("Found {} cases classified in {} classes."
-                     .format(self.case_number, self.class_number))
-        # create dataframe
+        log.info("Found {} cases classified in {} classes"
+                 .format(self.case_number, self.class_number))
         columns = ["main-class", "main-prob"]
         for i in range(self.class_number):
-            label = "prob-class-{}".format(i)
-            columns.append(label)
+            columns.append( "prob-class-{}".format(i) )
         self.stats = pd.DataFrame(np.nan,
                                   index=np.arange(1, self.case_number+1),
                                   columns=columns)
-        # second pass: fill dataframe
-        with open(case_name, 'r') as case_file:
+        # second pass: fill the dataframe
+        with open(case_name, "r") as case_file:
             for line in case_file:
                 if not line:
                     continue
-                if line.startswith('#') or line.startswith('DATA'):
+                if line.startswith("#") or line.startswith("DATA"):
                     continue
                 items = line.split()
                 assert len(items) >=0, \
@@ -96,12 +137,12 @@ class Output():
 
 
     @handle_error
-    def aggregate_input_data(self, datafile="clust.tsv"):
-        """
-        Aggregate autoclass classes with input data
+    def aggregate_input_data(self):
+        """Aggregate autoclass classes with input data
         """
         log.info("Aggregating input data")
-        self.df = pd.read_table(datafile, sep='\t', header=0, index_col=0)
+        intput_name = self.root_name + ".tsv"
+        self.df = pd.read_table(input_name, sep="\t", header=0, index_col=0)
         nrows, ncols = self.df.shape
         self.experiment_names = list(self.df.columns)
         assert len(self.stats.index) == nrows, \
@@ -114,8 +155,12 @@ class Output():
 
     @handle_error
     def write_cdt(self, with_proba=False):
-        """
-        Writing .cdt file for visualisation
+        """Write .cdt file for visualisation
+
+        Parameters
+        ----------
+        with_proba : bool (default False)
+            If True, also writes probability of case to belong to each class.
         """
         if not with_proba:
             log.info("Writing .cdt file")
@@ -136,10 +181,10 @@ class Output():
                                                          x["main-class"]+1),
                                        axis=1)
         # sort by increasing class
-        self.df.sort_values(by=['main-class', 'main-prob'],
+        self.df.sort_values(by=["main-class", "main-prob"],
                             ascending=[True, False],
                             inplace=True)
-        with open(filename, 'w') as cdtfile:
+        with open(filename, "w") as cdtfile:
             # write header line
             headers = ["GID", "UNIQID", "NAME", "GWEIGHT"]
             headers += self.experiment_names
@@ -173,17 +218,16 @@ class Output():
 
     @handle_error
     def write_cluster_stats(self):
-        """
-        Writing cluster stat file
+        """Writing cluster stat file
 
         Mean and standard deviation values per experiment
         """
         # count number of unique classes
-        class_max = self.df['main-class'].nunique()
-        with open('clust_stat.tsv', 'w') as statfile:
+        class_max = self.df["main-class"].nunique()
+        stat_name = self.root_name + "_stat.tsv"
+        with open(stat_name, "w") as statfile:
             # write headers
-            headers = ["cluster"] \
-                    + self.experiment_names
+            headers = ["cluster"] + self.experiment_names
             statfile.write("{}\n".format("\t".join(headers)))
             # write classes
             for class_idx in range(class_max):
@@ -199,13 +243,17 @@ class Output():
 
     @handle_error
     def wrap_outputs(self):
-        """
-        Wrap results into a ziped file
+        """Wrap results into a zipped file
+
+        Returns
+        -------
+        zipname : string
+            Name of the zip file that contains output files
         """
         t = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         zipname = "{}-autoclass-clust.zip".format(t)
         with zipfile.ZipFile(zipname, "w") as outputzip:
-            outputzip.write("clust.cdt")
-            outputzip.write("clust_withprobs.cdt")
-            outputzip.write("clust_stat.tsv")
+            outputzip.write(self.root_name + ".cdt")
+            outputzip.write(self.root_name + "_withprobs.cdt")
+            outputzip.write(self.root_name + "_stat.tsv")
         return zipname
