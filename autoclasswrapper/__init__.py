18bc05014f24c428c9ee9e2978318713f501d6c9
Pierre Poulain <pierre.poulain@cupnet.net>
2018-04-28 15:16:54 +0200

    Make version exposed in module

--- autoclasswrapper/__init__.py
+++ autoclasswrapper/__init__.py
@@ -3,11 +3,11 @@ log = logging.getLogger(__name__)
 log_handler = logging.StreamHandler()
 log_formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s",
                                   "%Y-%m-%d %H:%M:%S")
-# log_f = logging.Formatter('%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-#                           "%Y-%m-%d %H:%M:%S")
+
 log_handler.setFormatter(log_formatter)
 log.addHandler(log_handler)
 log.setLevel(logging.DEBUG)
 
 from .input import Input, Dataset, DuplicateColumnNameError
 from .output import Output
+from .__version__ import __version__
